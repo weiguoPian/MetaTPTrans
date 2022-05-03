524e755443bbcbe2c7fe9b09a14aca4b052e12ac
weiguoPian <798777547@qq.com>
2022-05-03 17:13:29 +0200

    update

--- run_completion.sh
+++ run_completion.sh
@@ -1,21 +1,21 @@
-CUDA_VISIBLE_DEVICES=0,1 python -u __main_completion__.py \
-                                --layers 5 \
-                                --e_ff_fold 2 \
-                                --attn_heads 8 \
-                                --hidden 1024 \
-                                --lan_embedding_dim 512 \
-                                --projection_dim 512 \
-                                --batch_size 64 \
-                                --accu_batch_size 128 \
-                                --val_batch_size 256 \
-                                --infer_batch_size 256 \
-                                --weight_decay 1e-4 \
-                                --lr 1e-4 \
-                                --min_lr 1e-6 \
-                                --factor 0.1 \
-                                --patience 2 \
-                                --dropout 0.2 \
-                                --epochs 40 \
-                                --MultiStepLR True \
-                                --milestones 25 35 \
-                                --lr_scheduler True
+python -u __main_completion__.py \
+        --layers 5 \
+        --e_ff_fold 2 \
+        --attn_heads 8 \
+        --hidden 1024 \
+        --lan_embedding_dim 512 \
+        --projection_dim 512 \
+        --batch_size 64 \
+        --accu_batch_size 128 \
+        --val_batch_size 256 \
+        --infer_batch_size 256 \
+        --weight_decay 1e-4 \
+        --lr 1e-4 \
+        --min_lr 1e-6 \
+        --factor 0.1 \
+        --patience 2 \
+        --dropout 0.2 \
+        --epochs 40 \
+        --MultiStepLR True \
+        --milestones 25 35 \
+        --lr_scheduler True
--- run_summarization.sh
+++ run_summarization.sh
@@ -1,20 +1,20 @@
-CUDA_VISIBLE_DEVICES=0,1 python -u __main_completion__.py \
-                                --layers 3 \
-                                --e_ff_fold 4 \
-                                --attn_heads 8 \
-                                --hidden 1024 \
-                                --lan_embedding_dim 1024 \
-                                --projection_dim 2048 \
-                                --batch_size 64 \
-                                --accu_batch_size 128 \
-                                --val_batch_size 256 \
-                                --infer_batch_size 256 \
-                                --weight_decay 1e-5 \
-                                --lr 1e-4 \
-                                --min_lr 1e-5 \
-                                --patience 0 \
-                                --dropout 0.2 \
-                                --epochs 20 \
-                                --MultiStepLR True \
-                                --milestones 25 35 \
-                                --lr_scheduler True
+python -u __main_completion__.py \
+        --layers 3 \
+        --e_ff_fold 4 \
+        --attn_heads 8 \
+        --hidden 1024 \
+        --lan_embedding_dim 1024 \
+        --projection_dim 2048 \
+        --batch_size 64 \
+        --accu_batch_size 128 \
+        --val_batch_size 256 \
+        --infer_batch_size 256 \
+        --weight_decay 1e-5 \
+        --lr 1e-4 \
+        --min_lr 1e-5 \
+        --patience 0 \
+        --dropout 0.2 \
+        --epochs 20 \
+        --MultiStepLR True \
+        --milestones 25 35 \
+        --lr_scheduler True
