dbc224aae5c16430c596f11815eed6ddaa463e45
weiguoPian <798777547@qq.com>
2022-11-21 03:10:33 +0100

    update

--- run_summarization.sh
+++ run_summarization.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 nohup python -u __main_summarization__.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python -u __main_summarization__.py \
         --model_type beta \
         --layers 3 \
         --e_ff_fold 4 \
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0,1 nohup python -u __main_summarization__.py \
         --hidden 1024 \
         --lan_embedding_dim 1024 \
         --projection_dim 2048 \
-        --batch_size 32 \
+        --batch_size 64 \
         --accu_batch_size 128 \
         --val_batch_size 256 \
         --infer_batch_size 256 \
